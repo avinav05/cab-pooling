69a59b9ee5155c1c83b8cdfb682f145da215f353
Priyam <priyamgupta340@gmail.com>
2020-06-08 19:02:06 +0530

    Chat Added

--- src/graphql/queries.js
+++ src/graphql/queries.js
@@ -1,5 +1,15 @@
-export const USER_EXISTS_QUERY = `
-query($email: String!) {
-  userExists(email: $email)
-}
+import gql from "graphql-tag";
+export const USER_EXISTS_QUERY = gql`
+  query($email: String!) {
+    userExists(email: $email)
+  }
+`;
+
+export const GET_CHAT_DETAILS = gql`
+  query getChat($link: String) {
+    getChat(link: $link) {
+      email
+      text
+    }
+  }
 `;
